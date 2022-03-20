efa6bfc538ebdd20a201dcb2970704607c9efe18
Mahlon Farbanish <farbanis@amazon.com>
2022-03-19 23:38:41 -0700

    Initial commit

---
+++ app/src/main/java/com/mahlon/photoapp/PhotoScrollActivity.kt
@@ -0,0 +1,11 @@
+package com.mahlon.photoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PhotoScrollActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.photo_scroll_activity)
+    }
+}
