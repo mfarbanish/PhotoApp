1b05e6cababceb6220ec40247842f6725b3c8177
Mahlon Farbanish <farbanis@amazon.com>
2022-04-24 01:33:01 -0400

    set up rough framework for loading images through glide

--- app/src/main/java/com/mahlon/photoapp/fragments/PhotoScrollAdapter.kt
+++ app/src/main/java/com/mahlon/photoapp/fragments/PhotoScrollAdapter.kt
@@ -3,10 +3,12 @@ package com.mahlon.photoapp.fragments
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.mahlon.photoapp.R
 import com.mahlon.photoapp.modal.UnsplashResponseItem
 
@@ -38,6 +40,11 @@ class PhotoScrollAdapter : RecyclerView.Adapter<PhotoScrollAdapter.PhotoScrollVi
     override fun onBindViewHolder(holder: PhotoScrollViewHolder, position: Int) {
         val item = listDiffer.currentList[position]
         holder.photoTitle.text = item.blur_hash
+
+        val imageUrl = item.urls.regular
+        Glide.with(holder.itemView)
+            .load(imageUrl)
+            .into(holder.image)
     }
 
     override fun getItemCount(): Int {
@@ -47,5 +54,6 @@ class PhotoScrollAdapter : RecyclerView.Adapter<PhotoScrollAdapter.PhotoScrollVi
     inner class PhotoScrollViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val photoTitle: TextView = itemView.findViewById(R.id.scroll_feed_row_text)
 
+        val image: ImageView = itemView.findViewById(R.id.row_photo_image_view)
     }
 }
